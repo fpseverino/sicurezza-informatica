9e3acfb115f8a0a97979ebf7417f4cf79fbb9df5
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2023-03-24 15:47:53 +0100

    Added file support in Vigenere

--- Vigenere/vigenere.c
+++ Vigenere/vigenere.c
@@ -1,22 +1,48 @@
+//
+//  vigenere.c
+//  Vigenere
+//
+//  Created by Francesco Paolo Severino on 07/03/23.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void printTable();
 
 char * encrypt(char * plainText, char * key);
 char * decrypt(char * cipherText, char * key);
+char * readTextFile(char * fileName);
+void writeTextFile(char * text, char * fileName);
 
-int main() {
-    printTable();
+int main(int argc, const char * argv[]) {
+    //printTable();
 
-    char plainText[] = "FRANCESCOPAOLOSEVERINO";
-    char key[64];
-    printf("Inserire chiave: ");
-    scanf("%s", key);
-    char * cipherText = encrypt(plainText, key);
-    printf("Cipher text: %s\n", cipherText);
-    printf("Testo decriptato: %s\n", decrypt(cipherText, key));
+    char * key = malloc(sizeof(char) * (strlen(argv[1]) + 1));
+    strcpy(key, argv[1]);
+    for (int i = 0; i < strlen(key); i++)
+        key[i] = toupper(key[i]) - 'A';
+    
+    char choice;
+    printf("Encrypt or decrypt? (e/d): ");
+    scanf("%c", &choice);
+
+    char * fileName = malloc(sizeof(char) * 255);
+    printf("File name: ");
+    scanf("%s", fileName);
+
+    if (choice == 'e') {
+        char * plainText = readTextFile(fileName);
+        char * cipherText = encrypt(plainText, key);
+        writeTextFile(cipherText, "ciphertext.txt");
+    } else if (choice == 'd') {
+        char * cipherText = readTextFile(fileName);
+        char * plainText = decrypt(cipherText, key);
+        writeTextFile(plainText, "plaintext.txt");
+    } else
+        printf("Invalid choice");
 
     return 0;
 }
@@ -53,4 +79,32 @@ char * decrypt(char * cipherText, char * key) {
     }
     plainText[cipherTextLength] = '\0';
     return plainText;
+}
+
+char * readTextFile(char * fileName) {
+    FILE * file = fopen(fileName, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    char * text = malloc(sizeof(char) * 1023);
+    int i = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        text[i] = c;
+        i++;
+    }
+    text[i] = '\0';
+    fclose(file);
+    return text;
+}
+
+void writeTextFile(char * text, char * fileName) {
+    FILE * file = fopen(fileName, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "%s", text);
+    fclose(file);
 }
