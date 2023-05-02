3ac2100fde3f52a6398da440ebe59585a9ec0c9d
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2023-05-02 09:14:19 +0200

    Changed name of GCD

---
+++ FourthGroup/gcd.swift
@@ -0,0 +1,39 @@
+//
+//  gcd.swift
+//  sicurezza-informatica
+//
+//  Created by Francesco Paolo Severino on 14/04/23.
+//
+
+import Foundation
+
+var a: Int
+var b: Int
+
+if CommandLine.argc != 3 {
+    print("Usage: euclide <number1> <number2>")
+    exit(1)
+}
+
+if let arg1 = Int(CommandLine.arguments[1]) {
+    a = arg1
+} else {
+    print("ERROR: First argument is not a number")
+    exit(1)
+}
+
+if let arg2 = Int(CommandLine.arguments[2]) {
+    b = arg2
+} else {
+    print("ERROR: Second argument is not a number")
+    exit(1)
+}
+
+var r: Int
+while b != 0 {
+    r = a % b
+    a = b
+    b = r
+}
+
+print("The GCD is \(a)")
